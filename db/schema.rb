af970ef2901c045164856c2f2fb31c5880117dcd
Jordan <jordansimons370@gmail.com>
2018-03-01 20:38:51 -0500

    Swapped from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180302005540) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -19,7 +22,7 @@ ActiveRecord::Schema.define(version: 20180302005540) do
     t.string "cuisine"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.string "photo_file_name"
     t.string "photo_content_type"
     t.integer "photo_file_size"
@@ -44,4 +47,5 @@ ActiveRecord::Schema.define(version: 20180302005540) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "restaurants", "users"
 end
